a9821211083debfceefd35172f047ff1d73b0428
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-01-09 17:37:31 +0100

    create input component

---
+++ components/ui/AmountInput.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import ErrorMessage from "@/components/ui/ErrorMessage";
+
+export default function AmountInput({ value, showError }: { value?: string; showError: boolean }) {
+	return (
+		<StyledDiv>
+			<StyledLabel htmlFor="amount">Amount</StyledLabel>
+			<StyledInput $showError={showError} id="amount" type="text" name="amount" defaultValue={value} placeholder="Please enter an amount" aria-describedby={showError ? "amount-error" : ""} aria-invalid={showError} />
+			{showError && <ErrorMessage id="amount-error" message="Please enter a valid amount" />}
+		</StyledDiv>
+	);
+}
+
+const StyledDiv = styled.div`
+	display: flex;
+	gap: var(--spacing-8);
+	flex-direction: column;
+`;
+const StyledLabel = styled.label`
+	color: var(--color-gray-50);
+	font: var(--font-regular-12);
+`;
+const StyledInput = styled.input<{ $showError: boolean }>`
+	height: var(--spacing-48);
+	font: var(--font-regular-14);
+	padding-left: var(--spacing-16);
+	border-radius: var(--spacing-8);
+	border: 1px solid ${(props) => (props.$showError ? "var(--color-red-90)" : "var(--color-gray-80)")};
+
+	&::placeholder {
+		color: var(--color-gray-50);
+	}
+`;
