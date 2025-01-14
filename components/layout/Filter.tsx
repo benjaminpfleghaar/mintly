3b3752945519597676b582dd5e65174e87f4cd85
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-01-14 18:15:06 +0100

    enable horizontal scroll for filter buttons

--- components/layout/Filter.tsx
+++ components/layout/Filter.tsx
@@ -1,19 +1,35 @@
 import styled from "styled-components";
 import { categories } from "@/data/categories";
 import PillButton from "@/components/ui/PillButton";
+import useEmblaCarousel from "embla-carousel-react";
 
 export default function Filter({ filter, handleFilter }: { filter: string; handleFilter: (category?: string) => void }) {
+	const [emblaRef] = useEmblaCarousel({ dragFree: true });
+
 	return (
-		<StyledDiv>
-			<PillButton label="All" onClick={() => handleFilter()} active={filter === ""} />
-			{categories.map((category) => (
-				<PillButton key={category} label={category} onClick={() => handleFilter(category)} active={filter === category} />
-			))}
-		</StyledDiv>
+		<StyledEmbla ref={emblaRef}>
+			<StyledEmblaContainer>
+				<StyledEmblaSlide>
+					<PillButton label="All" onClick={() => handleFilter()} active={filter === ""} />
+				</StyledEmblaSlide>
+				{categories.map((category) => (
+					<StyledEmblaSlide key={category}>
+						<PillButton label={category} onClick={() => handleFilter(category)} active={filter === category} />
+					</StyledEmblaSlide>
+				))}
+			</StyledEmblaContainer>
+		</StyledEmbla>
 	);
 }
 
-const StyledDiv = styled.div`
+const StyledEmbla = styled.div`
+	overflow: hidden;
+`;
+const StyledEmblaContainer = styled.div`
 	display: flex;
 	gap: var(--spacing-8);
+`;
+const StyledEmblaSlide = styled.div`
+	min-width: 0;
+	flex: 0 0 auto;
 `;
