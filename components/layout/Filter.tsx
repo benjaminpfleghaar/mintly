a48470b3494126415a5d178bd5edc23007cb020d
benjaminpfleghaar <info@benjaminpfleghaar.com>
2025-01-15 10:02:45 +0100

    add gradient to filter

--- components/layout/Filter.tsx
+++ components/layout/Filter.tsx
@@ -7,21 +7,35 @@ export default function Filter({ filter, handleFilter }: { filter: string; handl
 	const [emblaRef] = useEmblaCarousel({ dragFree: true });
 
 	return (
-		<StyledEmbla ref={emblaRef}>
-			<StyledEmblaContainer>
-				<StyledEmblaSlide>
-					<PillButton label="All" onClick={() => handleFilter()} active={filter === ""} />
-				</StyledEmblaSlide>
-				{categories.map((category) => (
-					<StyledEmblaSlide key={category}>
-						<PillButton label={category} onClick={() => handleFilter(category)} active={filter === category} />
+		<StyledFilter>
+			<StyledGradient></StyledGradient>
+			<StyledEmbla ref={emblaRef}>
+				<StyledEmblaContainer>
+					<StyledEmblaSlide>
+						<PillButton label="All" onClick={() => handleFilter()} active={filter === ""} />
 					</StyledEmblaSlide>
-				))}
-			</StyledEmblaContainer>
-		</StyledEmbla>
+					{categories.map((category) => (
+						<StyledEmblaSlide key={category}>
+							<PillButton label={category} onClick={() => handleFilter(category)} active={filter === category} />
+						</StyledEmblaSlide>
+					))}
+				</StyledEmblaContainer>
+			</StyledEmbla>
+		</StyledFilter>
 	);
 }
 
+const StyledFilter = styled.div`
+	position: relative;
+`;
+const StyledGradient = styled.div`
+	right: 0;
+	z-index: 1;
+	width: 32px;
+	height: 32px;
+	position: absolute;
+	background: linear-gradient(to right, rgba(22, 22, 22, 0), rgb(22, 22, 22));
+`;
 const StyledEmbla = styled.div`
 	overflow: hidden;
 `;
